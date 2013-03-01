d925308da05b6fe36d0d836e0a57775a1d547d9d
Raul Martinez <raul.martinez@kantox.com>
2013-03-01 12:08:58 +0100

    Custom file updated

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Buffer-menu-buffer+size-width 80)
- '(custom-safe-themes (quote ("eacf43271f6e4bf00654862e6071f87b8484f74066d5c9712fb217b32ba5677a" default)))
+ '(custom-safe-themes (quote ("159bb8f86836ea30261ece64ac695dc490e871d57107016c09f286146f0dae64" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "eacf43271f6e4bf00654862e6071f87b8484f74066d5c9712fb217b32ba5677a" default)))
  '(helm-buffer-max-length 60)
  '(helm-full-frame nil)
  '(rspec-use-bundler-when-possible nil)
@@ -22,7 +22,7 @@
  '(erm-syn-errline ((t (:underline "Red"))))
  '(erm-syn-warnline ((t (:underline "Orange"))))
  '(helm-selection ((t (:underline "Yellow"))))
- '(magit-diff-del ((t (:inherit diff-removed :foreground "#ff6666"))) nil "Custom diff color")
+ '(magit-diff-del ((t (:inherit diff-removed :foreground "ff6666"))))
  '(magit-item-highlight ((t (:inherit nil))))
  '(ruby-op-face ((t (:foreground "Magenta" :weight bold))))
  '(yas/field-highlight-face ((t (:inherit (quote highlight)))) t))
