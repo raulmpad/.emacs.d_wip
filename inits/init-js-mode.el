4811b96adfafab555a6e1059a9caa6096f30833e
Raul Martinez <raul.martinez@kantox.com>
2013-03-05 13:30:56 +0100

    Lintmode removed

--- inits/init-js-mode.el
+++ inits/init-js-mode.el
@@ -1,14 +1,14 @@
-(add-to-list 'load-path "~/.emacs.d/plugins/lintnode")
-
-(require 'flymake-jslint)
-;; Make sure we can find the lintnode executable
-(setq lintnode-location "~/.emacs.d/plugins/lintnode")
-;; JSLint can be... opinionated
-(setq lintnode-jslint-excludes (list 'nomen 'undef 'plusplus 'onevar 'white))
-;; Start the server when we first open a js file and start checking
-(add-hook 'js-mode-hook
-          (lambda ()
-            (lintnode-hook)))
+; (add-to-list 'load-path "~/.emacs.d/plugins/lintnode")
+; 
+; (require 'flymake-jslint)
+; ;; Make sure we can find the lintnode executable
+; (setq lintnode-location "~/.emacs.d/plugins/lintnode")
+; ;; JSLint can be... opinionated
+; (setq lintnode-jslint-excludes (list 'nomen 'undef 'plusplus 'onevar 'white))
+; ;; Start the server when we first open a js file and start checking
+; (add-hook 'js-mode-hook
+;           (lambda ()
+;             (lintnode-hook)))
 
 (add-hook 'js-mode-hook
           (lambda ()
